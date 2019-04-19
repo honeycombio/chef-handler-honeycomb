693b4cfe5e83ae296d933ef7309ccf3407cb547b
Martin Holman <me@martinholman.co.nz>
2019-04-19 15:06:51 -0700

    Fix class variables when events not called

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'ben@honeycomb.io'
 license          'All rights reserved'
 description      'Sets up Honeycomb report handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
+version          '0.1.1'
